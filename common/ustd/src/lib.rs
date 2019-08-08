94224a71182b5b8e06271401cc4afcf6e9623ebb
yangchaoqun <yangchaoqun@huobi.com>
2019-08-08 19:19:52 +0800

    feature .

--- common/ustd/src/lib.rs
+++ common/ustd/src/lib.rs
@@ -20,10 +20,8 @@
 // end::description[]
 
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(not(feature = "std"), no_std)]
-//#![cfg_attr(not(feature = "std"), feature(core_intrinsics))]
-//#![cfg_attr(not(feature = "std"), feature(alloc_prelude))]
+#![cfg_attr(not(feature = "std"), feature(core_intrinsics))]
+#![cfg_attr(not(feature = "std"), feature(alloc_prelude))]
 
 use cfg_if::cfg_if;
 
